3c4322ff121834f408f3c120f8cc731f7e182b64
Success David Praise <successdavidpraise99@gmail.com>
2025-08-13 23:36:07 +0100

    fixed again

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH ||
+  base: process.env.VITE_BASE_PATH || "/Portfolio-website"
   resolve: {
     alias:{
         "@" : path.resolve(__dirname, "./src"),
